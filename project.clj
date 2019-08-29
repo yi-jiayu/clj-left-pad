926374a5ba867f243e6c8c934717d5fbaff521c6
Jiayu Yi <yijiayu@gmail.com>
2019-08-29 13:11:43 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-left-pad "0.1.0-SNAPSHOT"
+  :description "String left pad"
+  :url "https://github.com/yi-jiayu/clj-left-pad"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories [["github" {:url "https://maven.pkg.github.com/yi-jiayu" :creds :gpg}]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns clj-left-pad.core})
